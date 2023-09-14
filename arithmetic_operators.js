fa22634a7d5b4586303914e66154a50dc03192cb
episkey2002 <pritirajunikam2411@gmail.com>
2023-09-14 11:44:30 +0530

    Js files

---
+++ arithmetic_operators.js
@@ -0,0 +1,25 @@
+//Declared two numeric variables having value 10 and 5
+let num1 = 10
+let num2 = 5
+//Declared variable result without assigning any value
+let result 
+
+//Addition
+result = num1 + num2
+console.log(result)
+
+//Multiplication
+result = num1 * num2
+console.log(result)
+
+//Subtraction
+result = num1 - num2
+console.log(result)
+
+//Division
+result = num1 / num2
+console.log(result)
+
+//Remainder
+result = num1 % num2
+console.log(result)
---
+++ conversion.js
@@ -0,0 +1,26 @@
+//Declared a variable num and assigned nnumber value 30 to it.
+let num = 30 //"=" id assignment operator
+
+console.log(num, typeof num)
+
+//convert number into a string
+let num1 = String(num)
+console.log(num1, typeof num1)
+
+let output_string = String (num)
+console.log(output_string, typeof output_string)
+
+//Declaring a variable num1 and assigned string value 123
+//let num1 = "123"
+//console.log(num1, typeof num1)
+
+//Convert String into number
+let output_number = Number(num1)
+console.log(output_number, typeof output_number)
+
+//  Boolean
+let num2 = 0
+console.log(num2, typeof num2)
+
+let output_bool = Boolean(num2)
+console.log(output_bool, typeof output_bool)
